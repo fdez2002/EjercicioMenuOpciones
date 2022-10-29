e5c0f7bbb781e2926d48bea5b5140949f6384c08
fdez2002 <fdez2002@gmail.com>
2022-10-29 17:53:13 +0200

    Creacion segunda pantalla

---
+++ app/src/main/java/com/example/ejerciciomenuopciones/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.ejerciciomenuopciones;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.ejerciciomenuopciones.databinding.ActivityMainBinding;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.menuDam:
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+                break;
+            case R.id.menuDaw:
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+                break;
+            case R.id.menuAsir:
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+                break;
+            default: return super.onOptionsItemSelected(item);
+
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
