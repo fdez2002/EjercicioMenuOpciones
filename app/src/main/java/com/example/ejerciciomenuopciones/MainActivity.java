5ac1c1643eb483858d65fa9856e22321d97bbc49
antonio <fdez2002@gmail.com>
2022-10-30 12:33:28 +0100

    Creacion menu al mantener pulsado en las imagenes

--- app/src/main/java/com/example/ejerciciomenuopciones/MainActivity.java
+++ app/src/main/java/com/example/ejerciciomenuopciones/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ejerciciomenuopciones.databinding.ActivityMainBinding;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
@@ -20,15 +23,57 @@ import com.google.android.material.snackbar.Snackbar;
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.imageView7.setImageResource(R.drawable.computer);
+        binding.imageView8.setImageResource(R.drawable.web);
+        binding.imageView9.setImageResource(R.drawable.linked);
+
+        registerForContextMenu(binding.imageView7);
+        registerForContextMenu(binding.imageView8);
+        registerForContextMenu(binding.imageView9);
+
+
+
+
+
 
     }
+    //Menu pulsado
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        getMenuInflater().inflate(R.menu.menu, menu);
+    }
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        Intent intent = new Intent(this, ScrollingActivity.class);
 
+        switch (item.getItemId()) {
+            case R.id.menuDam:
+                intent.putExtra("key", "CFGS DAM");
+
+                break;
+            case R.id.menuDaw:
+                intent.putExtra("key", "CFGS DAW");
+                break;
+            case R.id.menuAsir:
+                intent.putExtra("key", "CFGS ASIR");
+                break;
+            default: return super.onOptionsItemSelected(item);
+
+        }
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    //Menu toolbar
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
@@ -38,19 +83,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
+        Intent intent = new Intent(this, ScrollingActivity.class);
+
         switch (item.getItemId()) {
             case R.id.menuDam:
-                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+                intent.putExtra("key", "CFGS DAM");
+
                 break;
             case R.id.menuDaw:
-                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+                intent.putExtra("key", "CFGS DAW");
                 break;
             case R.id.menuAsir:
-
+                intent.putExtra("key", "CFGS ASIR");
                 break;
             default: return super.onOptionsItemSelected(item);
 
         }
+        startActivity(intent);
+
         return super.onOptionsItemSelected(item);
 
     }
--- app/src/main/java/com/example/ejerciciomenuopciones/ScrollingActivity.java
+++ app/src/main/java/com/example/ejerciciomenuopciones/ScrollingActivity.java
@@ -11,16 +11,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.ejerciciomenuopciones.databinding.ActivityScrollingBinding;
 
 public class ScrollingActivity extends AppCompatActivity {
 
     private ActivityScrollingBinding binding;
+    private TextView tv6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        tv6 = findViewById(R.id.tvTextoLArgo);
 
         binding = ActivityScrollingBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -28,7 +32,9 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = binding.toolbar;
         setSupportActionBar(toolbar);
         CollapsingToolbarLayout toolBarLayout = binding.toolbarLayout;
-        toolBarLayout.setTitle(getTitle());
+        toolBarLayout.setTitle(getIntent().getStringExtra("key"));
+
+
 
         FloatingActionButton fab = binding.fab;
         fab.setOnClickListener(new View.OnClickListener() {
@@ -37,5 +43,8 @@ public class ScrollingActivity extends AppCompatActivity {
                 startActivity(new Intent(ScrollingActivity.this, MainActivity.class));
             }
         });
+
+
+
     }
 }
